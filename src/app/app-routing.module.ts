37db353929a80da3cb8c05096f26a73f17745d6e
vGerJ02 <gjaimejuanguardia@gmail.com>
2024-05-02 16:52:55 +0200

    feat: add shelters list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserDeleteComponent } from './user/user-delete/user-delete.component';
+import { ShelterListComponent } from './shelter/shelter-list/shelter-list.component';
 
 const routes: Routes = [
   { path: 'users/create', component: UserRegisterComponent},
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'users/:id/edit', component: UserEditComponent, canActivate: [LoggedInGuard]},
   { path: 'users/:id', component: UserDetailComponent, canActivate: [LoggedInGuard]},
   { path: 'users', component: UserListComponent, canActivate: [LoggedInGuard]},
+  { path: 'shelters', component: ShelterListComponent },
   { path: 'about', component: AboutComponent},
   { path: '404', component: NotFoundComponent},
   { path: '', redirectTo: 'about', pathMatch: 'full'},
---
+++ src/app/shelter/shelter-list/shelter-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelter-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './shelter-list.component.html',
+  styleUrl: './shelter-list.component.css'
+})
+export class ShelterListComponent {
+
+}
