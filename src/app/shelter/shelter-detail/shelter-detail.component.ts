f098f06cd414ab10d29a75e7ce03bcac047e1b42
SebastianJitaru <sjitaru29@gmail.com>
2024-05-12 23:48:25 +0200

    Created shelter-details and service

---
+++ src/app/shelter/shelter-detail/shelter-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelter-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './shelter-detail.component.html',
+  styleUrl: './shelter-detail.component.css'
+})
+export class ShelterDetailComponent {
+
+}
