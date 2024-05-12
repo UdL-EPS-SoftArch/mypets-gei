384b31d014642c430c2f09fe79022bc6de5657c2
SebastianJitaru <sjitaru29@gmail.com>
2024-05-13 01:31:47 +0200

    Edit shelter component created

---
+++ src/app/shelter/shelter-edit/shelter-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelter-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './shelter-edit.component.html',
+  styleUrl: './shelter-edit.component.scss'
+})
+export class ShelterEditComponent {
+
+}
