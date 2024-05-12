d78233fae6b6a8139d99ac136d76c14e1063a4ab
SebastianJitaru <sjitaru29@gmail.com>
2024-05-13 01:17:18 +0200

    Shelter list implented

--- src/app/shelter/shelter-list/shelter-list.component.ts
+++ src/app/shelter/shelter-list/shelter-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ShelterData } from '../shelter-data';
+import { ShelterService } from '../shelter.service';
 @Component({
   selector: 'app-shelter-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './shelter-list.component.html',
   styleUrl: './shelter-list.component.css'
 })
-export class ShelterListComponent {
+export class ShelterListComponent implements OnInit {
+  shelters: ShelterData[];
 
+  constructor(private shelterService: ShelterService) { }
+
+  ngOnInit(): void {
+    this.getShelters();
+  }
+
+  async getShelters() {
+    this.shelters = await this.shelterService.getAllShelters();
+  }
 }
--- src/app/shelter/shelter.service.ts
+++ src/app/shelter/shelter.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { ShelterData } from './shelter-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShelterService {
+  protected shelters: ShelterData[] = [
+    {
+      id: 1,
+      name: 'Lleida Shelter',
+      email: 'shelter1@example.com',
+      mobile: '1234567890',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isActive: true,
+      rating: 4.5
+    },
+    {
+      id: 2,
+      name: 'Barcelona Shelter ',
+      email: 'shelter2@example.com',
+      mobile: '9876543210',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isActive: true,
+      rating: 3.8
+    },
+    {
+      id: 3,
+      name: 'Tarragona Shelter',
+      email: 'shelter3@example.com',
+      mobile: '5555555555',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isActive: false,
+      rating: 4.2
+    }
+  ]
 
-  constructor() { }
+  async getAllShelters(): Promise<ShelterData[]> {
+    return this.shelters;
+  }
 }
