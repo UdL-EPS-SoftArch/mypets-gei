db200d5a21ac4d09a58173cc0d26a6ff6a9f4d67
SebastianJitaru <sjitaru29@gmail.com>
2024-05-13 01:35:08 +0200

    Create shelter component created

---
+++ src/app/shelter/shelter-create/shelter-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelter-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './shelter-create.component.html',
+  styleUrl: './shelter-create.component.css'
+})
+export class ShelterCreateComponent {
+
+}
