b90aaf67b1a60a7d6c1309cc97b09c39e32c42d7
SebastianJitaru <sjitaru29@gmail.com>
2024-05-13 01:08:08 +0200

    delete component created

--- src/app/shelter/shelter-data.ts
+++ src/app/shelter/shelter-data.ts
@@ -1,3 +1,11 @@
 export interface ShelterData {
-    
-}
+    id: number;
+    name: string;
+    email: string;
+    mobile: string;
+    createdAt: Date;
+    updatedAt: Date;
+    isActive: boolean;
+    rating: number;
+  }
+  
---
+++ src/app/shelter/shelter-delete/shelter-delete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelter-delete',
+  standalone: true,
+  imports: [],
+  templateUrl: './shelter-delete.component.html',
+  styleUrl: './shelter-delete.component.css'
+})
+export class ShelterDeleteComponent {
+
+}
