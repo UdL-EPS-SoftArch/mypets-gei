152e424210c5debe23bfa6358572077877706c4a
peremunoz <peremunozfiguerol33@gmail.com>
2024-05-14 20:27:48 +0200

    Create certificate-add component

---
+++ src/app/shelter/certificate-add/certificate-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificate-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './certificate-add.component.html',
+  styleUrl: './certificate-add.component.scss'
+})
+export class CertificateAddComponent {
+
+}
