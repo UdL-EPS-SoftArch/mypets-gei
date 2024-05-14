6d9f0e70853279a59b848909e28d5b3cf7b5911b
Faysal Badaoui <faysalbadaoui02@gmail.com>
2024-05-14 19:39:19 +0200

    started logic to list volunteers

--- src/app/shelter/volunteer-list/volunteer-list.component.ts
+++ src/app/shelter/volunteer-list/volunteer-list.component.ts
@@ -4,28 +4,34 @@ import { User } from '../../login-basic/user';
 import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
 
 @Component({
-    selector: 'app-volunteer-list',
-    templateUrl: './volunteer-list.component.html'
-  })
-  export class ShelterVolunteersListComponent implements OnInit {
-    public users: User[] = [];
-    public pageSize = 5;
-    public page = 1;
-    public totalUsers = 0;
-  
-    constructor(
-      public router: Router) {
-    }
-  
-    ngOnInit(): void {
-     
-    }
-  
-    changePage(): void {
-      
-    }
-  
-    detail(user: User): void {
-      
-    }
+  selector: 'app-volunteer-list',
+  templateUrl: './volunteer-list.component.html'
+})
+export class ShelterVolunteersListComponent implements OnInit {
+  public users: User[] = [];
+  public pageSize = 5;
+  public page = 1;
+  public totalVolunteers = 0;
+
+  constructor(
+    public router: Router,
+    private userService: UserService) {
   }
+
+  ngOnInit(): void {
+    this.userService.getPage({ pageParams:  { size: this.pageSize }, sort: { username: 'ASC' } }).subscribe(
+        (page: PagedResourceCollection<User>) => {
+          this.users = page.resources;
+          this.totalVolunteers = page.totalElements;
+        });
+  }
+
+  changePage(): void {
+    this.userService.getPage({ pageParams: { page: this.page - 1, size: this.pageSize }, sort: { username: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<User>) => this.users = page.resources);
+  }
+
+  detail(user: User): void {
+    this.router.navigate(['volunteer', user.username]);
+  }
+}
