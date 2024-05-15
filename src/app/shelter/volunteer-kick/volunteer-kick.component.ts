eb2e249e8e8d8a17248f83e12b229c26fd3e4f26
Pol Escolà Curcó <polescola3000@gmail.com>
2024-05-15 17:49:11 +0200

    Kick volunteer from shelter created

---
+++ src/app/shelter/volunteer-kick/volunteer-kick.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-volunteer-kick',
+  templateUrl: './volunteer-kick.component.html',
+})
+export class VolunteerKickComponent {
+
+
+
+}
