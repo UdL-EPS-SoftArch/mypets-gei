82816653ec80a1e9875f6a3362355ba62d5eb905
Pol Escolà Curcó <polescola3000@gmail.com>
2024-05-15 17:51:52 +0200

    button created

--- src/app/shelter/volunteer-kick/volunteer-kick.component.ts
+++ src/app/shelter/volunteer-kick/volunteer-kick.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Volunteer } from '../../volunteer/volunteer';
+import { VolunteerService } from 'src/app/volunteer/volunteer.service';
 @Component({
   selector: 'app-volunteer-kick',
   templateUrl: './volunteer-kick.component.html',
 })
 export class VolunteerKickComponent {
+  public volunteer: Volunteer = new Volunteer();
+  public volunteer_to_kick: Volunteer = new Volunteer();
+  private id: string;
+  private id_to_kick: string;
+  
+  constructor(private route: ActivatedRoute,
+    private volunteerService: VolunteerService,) {
+}
 
+  kick(): void{
+    
+    this.volunteerService.deleteResource(this.volunteer_to_kick).subscribe(
+      () => {
+        
+      });
+  }
+
+  
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.volunteerService.getResource(this.id).subscribe(
+      volunteer => this.volunteer = volunteer);
+    this.id_to_kick = this.route.snapshot.paramMap.get('id_to_kick');
+    this.volunteerService.getResource(this.id_to_kick).subscribe(
+      volunteer_to_kick => this.volunteer_to_kick = volunteer_to_kick);
+  }
+  
 
 
 }
