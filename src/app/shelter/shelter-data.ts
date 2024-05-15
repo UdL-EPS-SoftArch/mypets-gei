fbfa416f606af7a0c84243503260dee5d51f68c7
vGerJ02 <gjaimejuanguardia@gmail.com>
2024-05-15 19:08:31 +0200

    fix: add uri field to shelter to get the id

--- src/app/shelter/shelter-data.ts
+++ src/app/shelter/shelter-data.ts
@@ -2,6 +2,7 @@ import { HateoasResource, Resource } from '@lagoshny/ngx-hateoas-client'
 
 @HateoasResource('shelters')
 export class Shelter extends Resource {
+  uri?: string
   name: string
   email: string
   mobile: string
