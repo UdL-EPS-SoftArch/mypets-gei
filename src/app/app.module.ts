cae1fd00b0a366b6b90b417361925990f325c4a6
Pol Escolà Curcó <polescola3000@gmail.com>
2024-05-15 17:53:06 +0200

    component added to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import {HttpErrorInterceptor} from './error-handler/http-error-interceptor';
 import {AuthenticationBasicService} from './login-basic/authentication-basic.service';
 import {LoggedInGuard} from './login-basic/loggedin.guard';
 import {UserService} from './user/user.service';
+import { VolunteerKickComponent } from './shelter/volunteer-kick/volunteer-kick.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import {UserService} from './user/user.service';
     UserRegisterComponent,
     UserEditComponent,
     UserDeleteComponent,
-    UserSearchComponent
+    UserSearchComponent,
+    VolunteerKickComponent,
   ],
   imports: [
     BrowserModule,
