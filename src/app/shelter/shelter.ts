cfd00fe448e7b170941fc1633fca15f79719b34d
Pol Escolà Curcó <polescola3000@gmail.com>
2024-05-16 17:12:46 +0200

    shelter class created

--- src/app/shelter/shelter.ts
+++ src/app/shelter/shelter.ts
@@ -2,5 +2,9 @@ import { HateoasResource, Resource } from '@lagoshny/ngx-hateoas-client';
 
 @HateoasResource('shelters')
 export class Shelter extends Resource {
-
+  name : string;
+  constructor(values: object = {}) {
+    super();
+    Object.assign(this as any, values);
+  }
 }
