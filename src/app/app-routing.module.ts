cb084cf7a42aa683ef5498b398609852ee522ae1
SebastianJitaru <sjitaru29@gmail.com>
2024-05-16 15:37:03 +0200

    Made delete pet page only visible to loggedin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent},
   { path: 'pets-grid', component: PetsGridComponent},
   { path: 'pet-details/:id', component:PetDetailsComponent,title:'Pet Details'},
-  { path: 'pet-details/:id/delete', component:PetDeleteComponent,title:'Pet Delete'},
+  { path: 'pet-details/:id/delete', component:PetDeleteComponent,title:'Pet Delete', canActivate: [LoggedInGuard]},
   { path: '404', component: NotFoundComponent},
   { path: '', redirectTo: 'about', pathMatch: 'full'},
   { path: 'pet-grid/add-pet', component: AddPetComponent, canActivate: [LoggedInGuard]}
