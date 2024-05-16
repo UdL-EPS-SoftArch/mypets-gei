d461ae1803104cb570f226d084743ff45bf27f94
Pol Escolà Curcó <polescola3000@gmail.com>
2024-05-16 17:14:01 +0200

    volunteer kick component retrieving shelter

--- src/app/shelter/volunteer-kick/volunteer-kick.component.ts
+++ src/app/shelter/volunteer-kick/volunteer-kick.component.ts
@@ -1,37 +1,45 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Volunteer } from '../../volunteer/volunteer';
 import { VolunteerService } from 'src/app/volunteer/volunteer.service';
+import { Shelter } from '../shelter';
+import { ShelterService } from '../shelter.service';
 @Component({
   selector: 'app-volunteer-kick',
   templateUrl: './volunteer-kick.component.html',
 })
-export class VolunteerKickComponent {
+export class VolunteerKickComponent implements OnInit{
+  public shelter: Shelter = new Shelter();
   public volunteer: Volunteer = new Volunteer();
-  public volunteer_to_kick: Volunteer = new Volunteer();
-  private id: string;
-  private id_to_kick: string;
+  private shleter_id: string;
+  private volunteer_id: string;
   
-  constructor(private route: ActivatedRoute,
-    private volunteerService: VolunteerService,) {
+  constructor(
+    private route: ActivatedRoute,
+    private volunteerService: VolunteerService,
+    private shleterService: ShelterService,
+    private router: Router,
+) {
 }
 
   kick(): void{
-    
-    this.volunteerService.deleteResource(this.volunteer_to_kick).subscribe(
+    this.volunteerService.deleteResource(this.volunteer).subscribe(
       () => {
-        
+        this.router.navigate(['/shelters'+this.shelter.name]);
       });
   }
 
   
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id');
-    this.volunteerService.getResource(this.id).subscribe(
-      volunteer => this.volunteer = volunteer);
-    this.id_to_kick = this.route.snapshot.paramMap.get('id_to_kick');
-    this.volunteerService.getResource(this.id_to_kick).subscribe(
-      volunteer_to_kick => this.volunteer_to_kick = volunteer_to_kick);
+    this.volunteer_id = this.route.snapshot.paramMap.get('volunteer');
+    this.volunteerService.getResource(this.volunteer_id).subscribe(
+      volunteer => {
+        this.volunteer = volunteer
+        this.volunteer.getRelation<Shelter>("userShelter").subscribe(
+          shelter => this.shelter = shelter
+        )
+      });
+      
   }
   
 
