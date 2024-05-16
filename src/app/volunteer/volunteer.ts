1521fee5a42db2d5a16490d107d1d39146c2ef2d
Pol Escolà Curcó <polescola3000@gmail.com>
2024-05-16 17:13:04 +0200

    volunteer refactored

--- src/app/volunteer/volunteer.ts
+++ src/app/volunteer/volunteer.ts
@@ -4,8 +4,8 @@ import { Shelter } from '../shelter/shelter';
 
 @HateoasResource('shelterVolunteers')
 export class Volunteer extends Resource {
-    userShelter = new Shelter()
-
+  //userShelter = new Shelter();
+  username = "";
   constructor(values: object = {}) {
     super();
     Object.assign(this as any, values);
