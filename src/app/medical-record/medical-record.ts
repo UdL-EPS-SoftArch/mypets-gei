0fe5763899c298e41e0ed7f71bfd414bbbe30fed
Codinab <alex.codinab@gmail.com>
2024-05-17 18:02:04 +0200

    Removed unused methods

--- src/app/medical-record/medical-record.ts
+++ src/app/medical-record/medical-record.ts
@@ -12,24 +12,6 @@ export class MedicalRecord extends Resource {
 
   constructor(values: object = {}) {
     super();
-    this.initialize(values);
-  }
-
-  initialize(values: object): void {
     Object.assign(this, values);
-    if (typeof this.uri === 'string') {
-      this.id = this.extractId(this.uri);
-    }
-  }
-
-  public set_id_from_uri(): void {
-    if (typeof this.uri === 'string') {
-      this.id = this.extractId(this.uri);
-    }
-  }
-
-  private extractId(uri: string): string {
-    const lastIndex = uri.lastIndexOf('/');
-    return lastIndex > -1 ? uri.substring(lastIndex + 1) : '';
   }
 }
