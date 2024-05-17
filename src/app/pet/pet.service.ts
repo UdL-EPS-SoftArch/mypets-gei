5c43ad7d8f5bf7da0463bdd1c709673225d84d12
SebastianJitaru <sjitaru29@gmail.com>
2024-05-17 18:54:16 +0200

    Added search functions to petservice

--- src/app/pet/pet.service.ts
+++ src/app/pet/pet.service.ts
@@ -15,6 +15,18 @@ export class PetService extends HateoasResourceOperation<Pet> {
   public findByIdContaining(query: string): Observable<ResourceCollection<Pet>> {
     return this.searchCollection('findByIdContaining', { params: { text: query } });
   }
+  public findBySize(size: string): Observable<ResourceCollection<Pet>> {
+    return this.searchCollection('findBySize', { params: { size } });
+  }
+  public findByName (name: string): Observable<ResourceCollection<Pet>> {
+    return this.searchCollection('findByName', { params: { name } });
+  }
+  public findByBreed (breed: string): Observable<ResourceCollection<Pet>> {
+    return this.searchCollection('findByBreed', { params: { breed } });
+  }
+  public findByIsAdopted (isAdopted: boolean): Observable<ResourceCollection<Pet>> {
+    return this.searchCollection('findByIsAdopted', { params: { isAdopted } });
+  }
   public getAllPets(): Observable<ResourceCollection<Pet>> {
     return this.getCollection();
   }
