c3180b5782b75aeaed2446760d568ba5450b8772
peremunoz <peremunozfiguerol33@gmail.com>
2024-05-18 14:06:22 +0200

    Implement functions to get certificates, validate and invalidate

--- src/app/shelter/certificate/certificate.service.ts
+++ src/app/shelter/certificate/certificate.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Certificate } from './certificate';
 import { HateoasResourceOperation, ResourceCollection } from '@lagoshny/ngx-hateoas-client';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,20 @@ export class CertificateService extends HateoasResourceOperation<Certificate> {
 
   constructor() {
     super(Certificate);
+  }
+
+  public getCertificates(): Observable<Certificate[]> {
+    return this.getPage().pipe(
+      map((resourceCollection: ResourceCollection<Certificate>) => resourceCollection.resources)
+    );
+  }
+
+  public validateCertificate(certificateId: string): Observable<Certificate> {
+    return this.patchResourceById(certificateId, { body: {validated: true} });
+  }
+
+  public invalidateCertificate(certificateId: string): Observable<Certificate> {
+    return this.patchResourceById(certificateId, { body: {validated: false} });
   }
 
 }
