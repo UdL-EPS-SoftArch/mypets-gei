c8fc1992b5fdc399a7a4ea18b0e8ebc526954363
peremunoz <peremunozfiguerol33@gmail.com>
2024-05-18 12:55:13 +0200

    Implement logic to add a shelter certificate

--- src/app/shelter/certificate-add/certificate-add.component.ts
+++ src/app/shelter/certificate-add/certificate-add.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ShelterService } from '../shelter.service';
+import { CertificateService } from '../certificate/certificate.service';
+import { Certificate } from '../certificate/certificate';
 
 @Component({
   selector: 'app-certificate-add',
   standalone: true,
-  imports: [],
+  imports: [NgbDatepickerModule, FormsModule],
   templateUrl: './certificate-add.component.html',
   styleUrl: './certificate-add.component.scss'
 })
-export class CertificateAddComponent {
+export class CertificateAddComponent implements OnInit {
 
+  constructor(
+    private route: ActivatedRoute,
+    private readonly shelterService: ShelterService,
+    private readonly certificateService: CertificateService
+  ) {}
+
+  ngOnInit(): void {
+    const shelterObservable = this.shelterService.getResource(this.shelterId);
+    shelterObservable.subscribe({
+      next: (shelter) => {
+        this.shelterName = shelter.name;
+      },
+      error: () => {
+        this.shelterName = 'Shelter not found';
+      }
+    });
+  }
+
+  
+  private modalService = inject(NgbModal);
+  protected shelterId: string = this.route.snapshot.params.id;
+  protected shelterName: string = 'None';
+  protected expirationDateObj: any = undefined
+  protected expirationDate: Date = undefined;
+	closeResult = '';
+
+  open(content: TemplateRef<any>) {
+		this.modalService.open(content, { ariaLabelledBy: 'add-shelter-certificate' })
+  }
+
+  addCertificate(modal) {
+    const year = this.expirationDateObj.year;
+    const month = this.expirationDateObj.month;
+    const day = this.expirationDateObj.day;
+    this.expirationDate = new Date(year, month, day);
+    const certificateDetails = new Certificate({
+      shelterId: `/shelter/${this.shelterId}`,
+      expirationDate: this.expirationDate,
+    });
+    if (this.expirationDate === undefined) {
+      alert('Please select an expiration date for the certificate');
+      return;
+    }
+    this.certificateService.createResource({body: certificateDetails}).subscribe({
+      next: () => {
+        alert('Certificate added successfully');
+        modal.close();
+      },
+      error: () => {
+        alert('Failed to add certificate');
+      }
+    });
+  }
 }
