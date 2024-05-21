db5ec4d4f40134283974e66acc84a8693b67c648
Roberto García <roberto@rhizomik.net>
2024-05-21 12:42:33 +0200

    Extract id failing when not yet fetched, use id from route

--- src/app/pet/pet-details/pet-details.component.ts
+++ src/app/pet/pet-details/pet-details.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
 import { PetService } from '../pet.service';
 import { Pet } from '../pet';
 import { PetFavouriteComponent } from '../pet-favourite/pet-favourite.component';
+
 @Component({
   selector: 'app-pet-details',
   standalone: true,
@@ -15,33 +16,20 @@ export class PetDetailsComponent implements OnInit {
   public pet: Pet = new Pet();
   petId: number | null = null;
 
-
   constructor(private petsService: PetService,
               private route: ActivatedRoute
   ) { }
   ngOnInit(): void {
-    this.fetchPet()
-    this.extractNumberFromUri(this.pet.uri);
+    this.fetchPet();
   }
 
   fetchPet(): void {
-    this.petsService.getResource(this.route.snapshot.params.id)
+    this.petId = this.route.snapshot.params.id;
+    this.petsService.getResource(this.petId)
       .subscribe({
         next: (response) => {
           this.pet = response;
         }
       });
-  }
-
-  extractNumberFromUri(uri: string): void {
-    const regex = /\/pets\/(\d+)/; // Regular expression to match digits after '/pets/'
-    const match = uri.match(regex); // Matching the URI with the regular expression
-
-    if (match && match.length > 1) {
-      this.petId = +match[1] // Extracting the number part and converting it to a number
-    } else {
-      // Handling case where the URI format doesn't match expected pattern
-      console.error("URI format doesn't match expected pattern.");
-    }
   }
 }
