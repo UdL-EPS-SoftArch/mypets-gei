f2b84ee9029b2beea876dcafb5b89f7a1d3fd44e
Irfankcss <114685092+Irfankcss@users.noreply.github.com>
2024-06-11 11:51:38 +0200

    conflicts with other tests fix

--- e2e/steps/add-pet.ts
+++ e2e/steps/add-pet.ts
@@ -1,13 +1,6 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 import { DataTable } from '@cucumber/cucumber';
 
-function loginAsUser(username: string, password: string) {
-  cy.get('.nav-link').contains('Login').click();
-  cy.get('#username').type(username).blur();
-  cy.get('#password').type(password).blur();
-  cy.get('button').contains('Submit').click();
-}
-
 function fillPetForm(table: DataTable) {
   table.rows().forEach((pair: string[]) => {
     const cyAttr = `pet-${pair[0].toLowerCase()}`;
@@ -20,17 +13,16 @@ Given('I\'m in the homepage', () => {
 });
 
 Given('I log in as {string} with password {string}', (username, password) => {
-  loginAsUser(username, password);
+  cy.get('.nav-link').contains('Login').click();
+  cy.get('#username').type(username).blur();
+  cy.get('#password').type(password).blur();
+  cy.get('button').contains('Submit').click();
 });
 
 Then('I\'m logged in as user {string}', (username) => {
   cy.get('#currentUser')
     .invoke('text')
     .should('contains', username);
-});
-
-Given('There is a registered admin with name {string} and password {string} and email {string}', (username, password, email) => {
-  cy.request('POST', 'http://localhost:8080/users', { username, password, email, role: 'admin' });
 });
 
 Given('I am in pets page', () => {
