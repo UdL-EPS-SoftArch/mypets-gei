1ac013860418ae8d45667141ccb0352756192fa5
Irfankcss <114685092+Irfankcss@users.noreply.github.com>
2024-06-12 18:48:35 +0200

    fix
    fixed undefined pet id in route by fetching a pet from medical record links since its not implemented in getall medical records functions. also get pets doesnt return petId so id was taken from uri tha tis unique for each pet

--- src/app/medical-record/medical-list/medical-record-list.component.ts
+++ src/app/medical-record/medical-list/medical-record-list.component.ts
@@ -6,32 +6,65 @@ import {Pet} from "../../pet/pet";
 import {CommonModule} from "@angular/common";
 import {Subscription} from "cypress/types/net-stubbing";
 import {AuthenticationBasicService} from "../../login-basic/authentication-basic.service";
+import {PetService} from "../../pet/pet.service";
 
 @Component({
-  selector: 'app-medical-record-list',
-  templateUrl: './medical-record-list.component.html',
-  standalone: true,
-  imports: [
-    RouterLink,
-    CommonModule,
-  ],
-  styleUrls: ['./medical-record-list.component.css']
+    selector: 'app-medical-record-list',
+    templateUrl: './medical-record-list.component.html',
+    standalone: true,
+    imports: [
+        RouterLink,
+        CommonModule,
+    ],
+    styleUrls: ['./medical-record-list.component.css']
 })
 export class MedicalRecordListComponent implements OnInit {
     medicalRecords: MedicalRecord[] = [];
+    petId: any;
+    selectedPet : Pet | undefined;
 
     constructor(private authenticationService: AuthenticationBasicService,
                 private medicalRecordService: MedicalRecordService,
-                private router: Router) { }
+                private petService: PetService,
+                private router: Router,
+                private route: ActivatedRoute,
+    ) { }
+
 
     ngOnInit(): void {
-        this.fetchMedicalRecords();
+
+
+        this.route.params.subscribe(params => {
+            console.log(params) //log the entire params object
+            console.log(params['petId']) //log the value of id
+
+            this.petId = params['petId'];
+        });
+
+
+
+
+        if (this.petId) {
+            this.fetchPet(this.petId);
+            console.log("petId", this.petId);
+        } else {
+            this.fetchMedicalRecords();
+            console.log("NO petId");
+        }
     }
 
-    fetchMedicalRecords() {
-        this.medicalRecordService.getMedicalRecords().subscribe({
+    fetchPet(petId: number) {
+        this.medicalRecordService.getMedicalRecordsByPetId(petId).subscribe({
             next: (records) => {
-                this.medicalRecords = records;
+                if (Array.isArray(records)) {
+                    records.forEach(record => {
+                        let lastIndex = record.uri.lastIndexOf('/');
+                        record.id = lastIndex > -1 ? record.uri.substring(lastIndex + 1) : '';
+                    })
+                    this.medicalRecords = records;
+                } else {
+                    console.error('Data returned is not an array:', records);
+                }
             },
             error: (error) => {
                 console.error('Failed to fetch medical records:', error);
@@ -40,14 +73,50 @@ export class MedicalRecordListComponent implements OnInit {
         });
     }
 
-    editRecord(record: MedicalRecord) {
-        console.log('Editing record:', record);
-        // Add navigation logic to edit the selected medical record
+    fetchMedicalRecords() {
+        this.medicalRecordService.getMedicalRecords().subscribe({
+            next: (records) => {
+                if (Array.isArray(records)) {
+                    records.forEach(record => {
+                        let lastIndex = record.uri.lastIndexOf('/');
+                        record.id = lastIndex > -1 ? record.uri.substring(lastIndex + 1) : '';
+                    })
+                    this.medicalRecords = records;
+                } else {
+                    console.error('Data returned is not an array:', records);
+                }
+            },
+            error: (error) => {
+                console.error('Failed to fetch medical records:', error);
+                this.medicalRecords = []; // Reset or handle as needed
+            }
+        });
     }
+    editRecord(record: any) {
+        const recordId = record.id;
+        const petUrl = record._links.pet.href;
+
+        this.petService.getPetByUrl(petUrl).subscribe({
+            next: (pet) => {
+                this.selectedPet = pet;
+                const lastIndex = pet.uri.lastIndexOf('/');
+                this.selectedPet.id = Number(lastIndex > -1 ? pet.uri.substring(lastIndex + 1) : '');
+                console.log(this.selectedPet, " - Selected Pet");
+
+
+                this.router.navigate([`/medical-records/${recordId}/${this.selectedPet.id}/edit`, {}]);
+            },
+            error: (error) => {
+                console.error('Failed to fetch pet:', error);
+                this.selectedPet = undefined;
+            }
+        });
+    }
+
 
     deleteRecord(recordId: string) {
         this.medicalRecordService.deleteMedicalRecord(recordId).subscribe(() => {
-            this.fetchMedicalRecords(); // Refresh the list after deletion
+            this.fetchMedicalRecords();
         });
     }
 
