4e3667c0e3795819b1899e66e5b591e0d04a6a14
peremunoz <peremunozfiguerol33@gmail.com>
2024-06-14 00:53:55 +0200

    Add all necessary steps definitions

--- e2e/steps/shelter-certificate.ts
+++ e2e/steps/shelter-certificate.ts
@@ -1,6 +1,18 @@
-import { Given, Then } from "cypress-cucumber-preprocessor/steps";
+import { And, Given, Then } from "cypress-cucumber-preprocessor/steps";
 
 
 Then('I can view the list of shelter certificates', () => {
     cy.get('app-certificate-validate').should('exist')
+})
+
+And('I fill the certificate date with a valid one', () => {
+    cy.get('#certificateExpirationDate').type('2099-01-01')
+})
+
+Then('The popup closes', () => {
+    cy.get('ngb-modal.window').should('not.exist')
+})
+
+And('The first shelter certificate is {string}', (status) => {
+    cy.get('#status').contains(status)
 })
