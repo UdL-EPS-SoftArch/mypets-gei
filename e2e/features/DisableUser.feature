8cce4ded6b3cb06c66a4a71d1aa5f663a2f60829
Faysal Badaoui <faysalbadaoui02@gmail.com>
2024-06-14 22:31:32 +0200

    first scenario done

--- e2e/features/DisableUser.feature
+++ e2e/features/DisableUser.feature
@@ -5,6 +5,7 @@ Feature: Disable User
 
     Scenario: View Users List
         Given I'm in the homepage
-        And I'm logged in as "admin" with password "password"
+        Given I log in as "admin" with password "password"
+        Then I'm logged in as user "admin"
         Given I am on the "Users" page
         Then I should see a list of users
--- e2e/steps/disable-user.ts
+++ e2e/steps/disable-user.ts
@@ -4,11 +4,19 @@ Given("I'm in the homepage", () => {
   cy.visit('/');
 });
 
-Given("I'm logged in as {string}", (username, password) => {
+Given('I log in as {string} with password {string}', (username, password) => {
+    cy.get('.nav-link').contains('Login').click();
+    cy.get('#username').type(username).blur();
+    cy.get('#password').type(password).blur();
+    cy.get('button').contains('Submit').click();
+  });
+
+  
+  Then('I\'m logged in as user {string}', (username) => {
     cy.get('#currentUser')
-    .invoke('text')
-    .should('contains', username);
-});
+      .invoke('text')
+      .should('contains', username);
+  });
 
 Given('I am on the {string} page', (page) => {
   cy.visit(`http://localhost:4200/${page.toLowerCase()}`);
